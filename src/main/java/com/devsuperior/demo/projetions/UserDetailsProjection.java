6ebce62306fe52dd90651758de7b7ac2381ef9a7
Marlon Marques <marlonmarques6@gmail.com>
2025-10-15 20:45:44 -0300

    implementado Roles no login

---
+++ src/main/java/com/devsuperior/demo/projetions/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.demo.projetions;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
+
